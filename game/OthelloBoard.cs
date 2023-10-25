71b842e9468e03912c3a69f596ce7216b8db33b0
joaoramirezz <joao.rkonrath@gmail.com>
2023-10-25 10:05:03 -0300

    bug fixes

--- game/OthelloBoard.cs
+++ game/OthelloBoard.cs
@@ -1,11 +1,13 @@
 public record struct OthelloBoard(ulong Winfo, ulong Binfo, byte Wcount, byte Bcount, byte Wplays)
 {
     ulong table = Winfo | Binfo, border;
-    int positions = [1, 9, -9, -1, -8, 8, 7, -7];
-    const ulong tableBorder = 18411139144890810879;
+    List<int> positions = new List<int>{1, 9, -9, -1, -8, 8, 7, -7};
+
+    
+    ulong tableBorder = 18411139144890810879;
     public void GetBorderGame()
     {
-        if(!(table & tableBorder))
+        if((table & tableBorder) > 0)
         {
             for(int i = 0; i < 8; i++)
                 border |= table >>> positions[i];
@@ -15,28 +17,28 @@ public record struct OthelloBoard(ulong Winfo, ulong Binfo, byte Wcount, byte Bc
 
     public ulong PlaysAvailable()
     {
-        ulong = tempBorder, findEnemy, findAlly, playAvailable ;
-        if(Wplays)
+        ulong tempBorder, findEnemy, findAlly, playAvailable = 0;
+        if(Wplays == 1)
         {
 
             ulong temMap = border | Winfo;
-            for(int i = 0; i < 64; i++)
+            for(int j = 0; j < 64; j++)
             {
-                tempBorder = border >>> 63 - i;
-                if(!(tempBorder & 1 == 1))
+                tempBorder = border >>> 63 - j;
+                if(!((tempBorder & 1 )== 1))
                     continue;
 
                 for(int i = 0; i < 8; i++)
                 {
                     findEnemy = tempBorder >>> positions[i];
-                    if(!(findEnemy & 1 == 1))
+                    if(!((findEnemy & 1 )== 1))
                         continue;
-                    if(!(findEnemy & Binfo > 0))
+                    if(!((findEnemy & Binfo )> 0))
                         continue;
-                    while(findEnemy & tableBorder != 1)
+                    while((findEnemy & tableBorder) != 1)
                     {
                         findAlly = findEnemy >>> positions[i];
-                        if(findAlly & Winfo > 0)
+                        if((findAlly & Winfo) > 0)
                             playAvailable |= tempBorder;
                     } 
                 }
@@ -44,23 +46,23 @@ public record struct OthelloBoard(ulong Winfo, ulong Binfo, byte Wcount, byte Bc
         }
         else
         {
-            for(int i = 0; i < 64; i++)
+            for(int j = 0; j < 64; j++)
             {
-                tempBorder = border >>> 63 - i;
-                if(!(tempBorder & 1 == 1))
+                tempBorder = border >>> 63 - j;
+                if(!((tempBorder & 1 )== 1))
                     continue;
 
                 for(int i = 0; i < 8; i++)
                 {
                     findEnemy = tempBorder >>> positions[i];
-                    if(!(findEnemy & 1 == 1))
+                    if(!((findEnemy & 1 )== 1))
                         continue;
-                    if(!(findEnemy & Winfo > 0))
+                    if(!((findEnemy & Winfo )> 0))
                         continue;
-                    while(findEnemy & tableBorder != 1)
+                    while((findEnemy & tableBorder) != 1)
                     {
                         findAlly = findEnemy >>> positions[i];
-                        if(findAlly & Binfo > 0)
+                        if((findAlly & Binfo) > 0)
                             playAvailable |= tempBorder;
                     } 
                 }
